84d650dc5e5b28a1442a6db6335627fb6e7e6ab0
eoinmurray <amonecho1@gmail.com>
2016-01-13 13:50:07 +0000

    edit

--- _site/deploy.sh
+++ _site/deploy.sh
@@ -1,6 +1,6 @@
+jekyll build
 git add .
 git commit -m "edit"
 git push origin master
 git add _site && git commit -m "Initial dist subtree commit"
 git subtree push --prefix _site origin gh-pages
-  
